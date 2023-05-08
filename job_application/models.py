e9711f910223c0a3c65b50eb31130561b91288d9
sukshan <sukshanj@gmail.com>
2023-05-08 14:06:51 +0530

    resolved circular dependency between job and job_application

--- job_application/models.py
+++ job_application/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from users.models import users
-from job.models import job
+
+
 
 class job_application(models.Model):
-    user_id = models.ForeignKey(users, on_delete=models.CASCADE)
-    job_id = models.ForeignKey(job, on_delete=models.CASCADE)
+    user_id= models.ForeignKey(users, on_delete=models.CASCADE)
+    job_id = models.ForeignKey('job.job', on_delete=models.CASCADE)
+
+# class job_application(models.Model):
+#     user_id = models.ForeignKey(users, on_delete=models.CASCADE)
+#     job_id = models.ForeignKey(job, on_delete=models.CASCADE)
 
 # Create your models here.
